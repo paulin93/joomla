c7fd5c85202961f30b27b9a03c4b3d9b89b6df07
Paulina Tyszka <p.tyszka@op.pl>
2015-04-14 12:30:47 +0200

    Joomla

---
+++ logs/error.php
@@ -0,0 +1,10 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-03-25 13:38:32 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2015-03-25	13:38:32	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-03-25	13:40:04	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
+2015-04-13	18:07:36	INFO	127.0.0.1	Joomla FAILURE: 	Nieprawidłowa nazwa użytkownika lub hasło albo nie masz u nas jeszcze konta
+2015-04-14	09:57:41	INFO	127.0.0.1	Joomla FAILURE: 	Puste hasła są niedozwolone
